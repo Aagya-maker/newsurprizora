c4e4068ef2db9e2ca2fa39aa204e524780f6a6c9
Aagya <avisstarr12@gmail.com>
2024-06-07 17:29:35 +0545

    slides

--- src/app/page.js
+++ src/app/page.js
@@ -1,50 +1,89 @@
 'use client';
-
-import React from 'react';
+import { useEffect } from 'react';
+import Link from 'next/link';
 import useEmblaCarousel from 'embla-carousel-react';
 import Autoplay from 'embla-carousel-autoplay';
+import SmoothScroll from 'smooth-scroll';
 
 export default function EmblaCarousel() {
     const [emblaRef] = useEmblaCarousel({ loop: true }, [
         Autoplay({ delay: 4000 }),
     ]);
 
+    useEffect(() => {
+       
+        const scroll = new SmoothScroll('a[href*="#"]', {
+            speed: 10000,
+            speedAsDuration: true,
+            easing: 'easeInOutQuint', 
+            offset: 0, 
+        });
+
+        return () => {
+            scroll.destroy();
+        };
+    }, []);
+
     return (
         <div>
-            <h1 className='text-center font-bold mb-4'>Surprizora</h1>
-            <div
-                className='embla mx-auto mt-20 h-128 max-w-6xl border-collapse'
-                ref={emblaRef}
-            >
-                <div className='embla__container h-full'>
-                    <div className='embla__slide flex items-center justify-center'>
-                        <img
-                            src='surprizora.png'
-                            alt='Welcome to surprizora'
-                            className='w-full h-full object-cover'
-                        />
-                    </div>
-                    <div className='embla__slide flex items-center justify-center'>
-                        <img
-                            src='surprizora 2.png'
-                            alt='slide 2'
-                            className='w-full h-full object-cover'
-                        />
-                    </div>
-                    <div className='embla__slide flex items-center justify-center'>
-                        <img
-                            src='surprizora 6.png'
-                            alt='slide 2'
-                            className='w-full h-full object-cover'
-                        />
+            <section id="page-top" className='h-screen'>
+                <h1 className='text-center font-bold mb-1'>Surprizora</h1>
+                <div
+                    className='embla mx-auto mt-20 h-128 max-w-6xl border-collapse'
+                    ref={emblaRef}
+                >
+                    <div className='embla__container h-full'>
+                        <div className='embla__slide flex items-center justify-center'>
+                            <img
+                                src='surprizora.png'
+                                alt='Welcome to surprizora'
+                                className='w-full h-full object-cover'
+                            />
+                        </div>
+                        <div className='embla__slide flex items-center justify-center'>
+                            <img
+                                src='surprizora 2.png'
+                                alt='slide 2'
+                                className='w-full h-full object-cover'
+                            />
+                        </div>
+                        <div className='embla__slide flex items-center justify-center'>
+                            <img
+                                src='surprizora 6.png'
+                                alt='slide 2'
+                                className='w-full h-full object-cover'
+                            />
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div className='text-center mt-8'>
-                <button className='bg-purple-500 hover:bg-purple-700 font-bold py-2 px-4 rounded-full border-b-8'>
-                    Take the questionnaire and begin the gift quest
-                </button>
-            </div>
+                <div className='text-center mt-8'>
+                    <Link href="#question-section">
+                        <button
+                            className='bg-purple-500 hover:bg-purple-700 font-bold py-2 px-4 rounded-full border-b-8'
+                        >
+                            Take the questionnaire and begin the gift quest
+                        </button>
+                    </Link>
+                </div>
+            </section>
+
+            <section id="question-section" className='max-w-6xl mx-auto mt-20 p-4 bg-silver-300 h-screen'>
+                <h1 className='text-center font-bold mb-4'>Questionnaire</h1>
+                <div>
+                    <div className='mt-20'>
+                        <p>This is the questionnaire section</p>
+                    </div>
+                </div>
+                <div className='text-center mt-8'>
+                    <Link href="#page-top">
+                        <button
+                            className='bg-purple-500 hover:bg-purple-700 font-bold py-2 px-4 rounded-full border-b-8'
+                        >
+                            Back to top
+                        </button>
+                    </Link>
+                </div>
+            </section>
         </div>
     );
 }
